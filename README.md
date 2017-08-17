3502aba3c16abb4a603ab9301e442f92e599b066
Vitor Cavalcanti <vitorluizc@outlook.com>
2017-08-17 11:07:10 -0300

    rename "formaters" to "formatters" (fix #3) + add install JSDoc

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ import Vue from 'vue'
 import Util from 'vue-convenia-util'
 
 Vue.use(Util, {
-  formaters: true,
+  formatters: true,
   formatFilters: true
 })
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import * as $format from './formaters'
+import * as $format from './formatters'
 import * as $validation from './validations'
 
 export { $format as format }
 
 export { $validation as validation }
 
+/**
+ * Opções do plugin.
+ * @typedef {Object} Options
+ * @property {Boolean} formatters
+ * @property {Boolean} formatFilters
+ * @property {Boolean} validations
+ */
+
+/**
+ * Adiciona as funções auxiliares definidas no protótipo do Vue, e
+ * consequentemente aos componentes.
+ * @param {Vue} Vue
+ * @param {Options} options
+ */
 const install = (Vue, options = {}) => {
-  if (options.formaters) {
+  if (options.formatters) {
     Vue.prototype.$format = $format
   }
 
