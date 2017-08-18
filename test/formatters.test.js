55694d5d9e91b6ad96daed0cc167db96a0f027a9
Vitor Cavalcanti <vitorluizc@outlook.com>
2017-08-18 16:43:56 -0300

    add unit tests

--- test/formatters.test.js
+++ test/formatters.test.js
@@ -1,14 +1,77 @@
 import test from 'ava'
 import { format } from '../'
 
-test('toCPF: Formata um CPF válido', (context) => {
+test('toCPF: Formata um valor, ou parte dele para CPF', (context) => {
   context.is(format.toCPF('00000000000'), '000.000.000-00')
-})
-
-test('toCPF: Formata parte de um CPF', (context) => {
   context.is(format.toCPF('00000000'), '000.000.00')
+  context.is(format.toCPF('366.418.768-70'), '366.418.768-70')
 })
 
-test('toRG: Formata parte de um CPF', (context) => {
-  context.is(format.toRG('00000000'), '00.000.000')
+test('toCPF: Não formata valores inválidos', (context) => {
+  context.is(format.toCPF('Abacaxi'), null)
+  context.is(format.toCPF(undefined), null)
+  context.is(format.toCPF(null), null)
+})
+
+test('toRG: Formata um valor, ou parte dele, para RG', (context) => {
+  context.is(format.toRG('000000000'), '00.000.000-0')
+  context.is(format.toRG('000000'), '00.000.0')
+})
+
+test('toRG: Não formata valores inválidos', (context) => {
+  context.is(format.toRG('Abacaxi'), null)
+  context.is(format.toRG(undefined), null)
+  context.is(format.toRG(null), null)
+})
+
+test('toMoney: Formata um valor para moeda', (context) => {
+  context.is(format.toMoney(1200.504), 'R$ 1.200,50')
+  context.is(format.toMoney(0), 'R$ 0,00')
+  context.is(format.toMoney('-74.89'), 'R$ -74,89')
+})
+
+test('toMoney: Não formata valores inválidos', (context) => {
+  context.is(format.toMoney(null), null)
+  context.is(format.toMoney(undefined), null)
+  context.is(format.toMoney('XXI'), null)
+  context.is(format.toMoney([]), null)
+})
+
+test('toYears: Obtém em anos a diferença de uam data', (context) => {
+  context.is(format.toYears('28/03/1996'), 21)
+  context.is(format.toYears('1979-04-13'), 38)
+})
+
+test('toYears: Não formata valores inválidos', (context) => {
+  context.is(format.toYears(null), null)
+  context.is(format.toYears(undefined), null)
+  context.is(format.toYears('21/15/2017'), null)
+})
+
+test('toDate: Formata uma data (DD-MM-YYYY | YYYY-MM-DD) para DD/MM/YYYY', (context) => {
+  context.is(format.toDate('21/08/2002'), '21/08/2002')
+  context.is(format.toDate('21-08-2002'), '21/08/2002')
+  context.is(format.toDate('2002-08-21'), '21/08/2002')
+  context.is(format.toDate('2002-08-21'), '21/08/2002')
+  context.is(format.toDate('2002-08-21'), '21/08/2002')
+})
+
+test('toDate: Com o toDatabase formata (DD-MM-YYYY | DD/MM/YYYY) para YYYY-MM-DD', (context) => {
+  context.is(format.toDate('21/08/2002', true), '2002-08-21')
+  context.is(format.toDate('21-08-2002', true), '2002-08-21')
+  context.is(format.toDate('2002-08-21', true), '2002-08-21')
+})
+
+test('toDate: Não formata valores inválidos', (context) => {
+  context.is(format.toDate(null), null)
+  context.is(format.toDate(undefined), null)
+  context.is(format.toDate('21/08/01'), null)
+  context.is(format.toDate('21/30/2001'), null)
+})
+
+test('toEmpty: Retorna um caractere vazio para um dado vazio', (context) => {
+  context.is(format.toEmpty(null), '-')
+  context.is(format.toEmpty(undefined, '*'), '*')
+  context.is(format.toEmpty(''), '-')
+  context.is(format.toEmpty(NaN), '-')
 })
--- test/validators.test.js
+++ test/validators.test.js
@@ -1,21 +1,58 @@
 import test from 'ava'
 import { validate } from '../'
 
+test('is: Verdadeiro para a comparação de um valor com seu construtor', (context) => {
+  context.is(validate.is(null, 'Null'), true)
+  context.is(validate.is(undefined, 'Undefined'), true)
+  context.is(validate.is(1200, 'Number'), true)
+  context.is(validate.is('R$ 120,20', 'String'), true)
+  context.is(validate.is([0, 1, 2, 3], 'Array'), true)
+  context.is(validate.is({ name: 'Convenia' }, 'Object'), true)
+})
+
 test('isCPF: Verdadeiro para um CPF válido', (context) => {
   context.is(validate.isCPF('366.418.768-70'), true)
-})
-
-test('isCPF: Falso para um CPF inválido', (context) => {
+  context.is(validate.isCPF('36641876870'), true)
   context.is(validate.isCPF('213.198.013-20'), false)
+  context.is(validate.isCPF('2131201872781'), false)
+  context.is(validate.isCPF('11111111111'), true)
+  context.is(validate.isCPF('00000000000'), false)
+  context.is(validate.isCPF('111.111.111-11'), true)
+  context.is(validate.isCPF('UHASHUISIH910'), false)
 })
 
-test('isCPF: Falso para algo que não "string"', (context) => {
+test('isCPF: Falso para dados que não sejam "string"', (context) => {
   // Teste que valida a correção do issue #5
   // Link: https://github.com/convenia/vue-convenia-util/issues/5
   context.is(validate.isCPF(undefined), false)
   context.is(validate.isCPF(null), false)
   context.is(validate.isCPF(36641876870), false)
-  context.is(validate.isCPF(true), false)
-  context.is(validate.isCPF({}), false)
-  context.is(validate.isCPF([]), false)
+})
+
+test('isDate: Verdadeiro para uma data válida (DD/MM/YYYY | DD-MM-YYYY & YYYY-MM-DD)', (context) => {
+  context.is(validate.isDate('28/03/2017'), true)
+  context.is(validate.isDate('28-03-2017'), true)
+  context.is(validate.isDate('2017-03-28'), true)
+  context.is(validate.isDate('31/02/2017'), false)
+})
+
+test('isDate: Falso para uma data inválida ou fora da formatação padrão', (context) => {
+  context.is(validate.isDate('03/28/2017'), false)
+  context.is(validate.isDate('28-13-9017'), false)
+  context.is(validate.isDate('0000-00-00'), false)
+  context.is(validate.isDate('29/12'), false)
+})
+
+test('isDate: Valida usando o formato especificado ao invés de um dinâmico', (context) => {
+  context.is(validate.isDate('2017-03', 'YYYY-MM'), true)
+  context.is(validate.isDate('31/13/17', 'DD/MM/YY'), false)
+  context.is(validate.isDate('28 02', 'DD MM'), true)
+})
+
+test('isCPF: Falso para dados que não sejam "string"', (context) => {
+  // Teste que valida a correção do issue #5
+  // Link: https://github.com/convenia/vue-convenia-util/issues/5
+  context.is(validate.isDate(undefined), false)
+  context.is(validate.isDate(null), false)
+  context.is(validate.isDate(36641876870), false)
 })
