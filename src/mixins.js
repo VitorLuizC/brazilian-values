85e38dd4ca7482b1142a2fa4f55cd87deba9e4ac
Vitor Cavalcanti <vitorluizc@outlook.com>
2017-10-03 10:59:21 -0300

    Fix Loadable optional action + :lipstick:

--- src/mixins.js
+++ src/mixins.js
@@ -2,22 +2,17 @@
  * Adiciona o dado isLoading com true, que assim que o componente é montado e a
  * action é executada ele passa a ser false.
  * @param {function(Vue): Promise} action
- * @returns {{ data: function():{ isLoading: Boolean } mounted: function}}
  */
-export const Loadable = (action) => {
-  const Loadable = {
-    data () {
-      return {
-        isLoading: true
-      }
-    },
-    mounted () {
-      (action(this) || Promise.resolve())
-        .then(() => {
-          this.isLoading = false
-        })
+export const Loadable = (action) => ({
+  data () {
+    return {
+      isLoading: true
     }
+  },
+  mounted () {
+    (action ? action(this) : Promise.resolve())
+      .then(() => {
+        this.isLoading = false
+      })
   }
-
-  return Loadable
-}
+})
