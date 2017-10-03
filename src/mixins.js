2d84a4b84dc0e76b7da49c509243189bfbf756e3
Vitor Cavalcanti <vitorluizc@outlook.com>
2017-10-03 11:00:25 -0300

    Add ObservableFix mixin

--- src/mixins.js
+++ src/mixins.js
@@ -16,3 +16,35 @@ export const Loadable = (action) => ({
       })
   }
 })
+
+/**
+ * Resolve o problema das propriedades não inicializadas que não poderiam ser
+ * observadas.
+ * @param {{}} template
+ */
+export const ObservableFix = (template = {}) => ({
+  props: {
+    data: {
+      type: Object,
+      default: () => Object.assign({}, template)
+    }
+  },
+  data () {
+    return {
+      selected: Object.assign({}, template)
+    }
+  },
+  watch: {
+    data () {
+      this.cloneData()
+    }
+  },
+  methods: {
+    cloneData () {
+      this.selected = Object.assign({}, template, this.data)
+    }
+  },
+  mounted () {
+    this.cloneData()
+  }
+})
