ee1c5387d749c82e587b6941368823ae2bcac932
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-03-10 16:30:39 -0300

    :bug: fix parsers index

--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -1,6 +1,3 @@
-export {
-  default as parseArrayToStringWithAticle
-} from "./parseArrayToStringWithAticle";
 export { default as parseToArray } from './parseToArray';
 export { default as parseToDate } from './parseToDate';
 export { default as parseToNumber } from './parseToNumber';
