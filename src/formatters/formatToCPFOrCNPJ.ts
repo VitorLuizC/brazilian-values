8988b6b62da3b40a77dd9cc48204a86375cb920d
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-02-15 18:49:51 -0300

    :sparkles: keep consistent code-style & splits function concerns

--- src/formatters/formatToCPFOrCNPJ.ts
+++ src/formatters/formatToCPFOrCNPJ.ts
@@ -1,24 +1,26 @@
-import formatToCNPJ from "./formatToCNPJ";
-import formatToCPF from "./formatToCPF";
-import mapToNumeric from '../helpers/mapToNumeric';
+import formatToCNPJ from './formatToCNPJ';
+import formatToCPF from './formatToCPF';
 
 /**
- * Formats a `string` value into a CPF or CNPJ depending on the size of it
+ * Check if a `string` value can be formatted to CPF.
+ * @param value - A `string` value of a CPF or CNPJ.
+ */
+const canFormatToCPF = (
+  value: string,
+): boolean => (value.match(/\d/g)?.length ?? 0) <= 11
+
+/**
+ * Formats step-by-step a `string` value to CPF or CNPJ depending on its length.
+ * @example
  * formatToCPFOrCNPJ('00000000000')
  * //=> '000.000.000-00'
  *
  * formatToCPFOrCNPJ('36641876870')
  * //=> '366.418.768-70'
- * 
- * @param value - A `string` value of a CPF or CNPJ. 
+ * @param value - A `string` value of a CPF or CNPJ.
  */
 const formatToCPFOrCNPJ = (
-    value: string
-) => {
-    if (mapToNumeric(value).length <= 11){
-        return formatToCPF(value);
-    }
-    return formatToCNPJ(value);
-};
+  value: string,
+): string => canFormatToCPF(value) ? formatToCPF(value) : formatToCNPJ(value);
 
 export default formatToCPFOrCNPJ;
