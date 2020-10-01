69e41ac43a2a773dee5ce9253cced959e019265a
Rogério Munhoz <roz@rjmunhoz.me>
2020-10-01 14:35:22 -0300

    :white_check_mark: Add tests for isCPFOrCNPJ

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -4,3 +4,4 @@ export { default as isCPF } from './isCPF';
 export { default as isDate } from './isDate';
 export { default as isDDD } from './isDDD';
 export { default as isPhone } from './isPhone';
+export { default as isCPFOrCNPJ } from './isCPFOrCNPJ';
--- test/validators.test.ts
+++ test/validators.test.ts
@@ -5,7 +5,8 @@ import {
   isCPF,
   isDate,
   isDDD,
-  isPhone
+  isPhone,
+  isCPFOrCNPJ,
 } from '../src/brazilian-values';
 
 test('isCEP', (context) => {
@@ -37,6 +38,23 @@ test('isCPF', (context) => {
   context.false(isCPF('UHASHUISIH910'));
   context.false(isCPF('366.41askaosoa8.768-70'));
 });
+
+test('isCPFOrCNPJ', (context) => {
+  context.true(isCPFOrCNPJ("41142260000189"));
+  context.true(isCPFOrCNPJ("45.723.174/0001-10"));
+  context.false(isCPFOrCNPJ("41142260007182"));
+  context.false(isCPFOrCNPJ("19.981.127/0001-10"));
+  context.false(isCPFOrCNPJ("64.637.agsvs009/0001-90"));
+  context.true(isCPFOrCNPJ("366.418.768-70"));
+  context.true(isCPFOrCNPJ("36641876870"));
+  context.false(isCPFOrCNPJ("213.198.013-20"));
+  context.false(isCPFOrCNPJ("2131201872781"));
+  context.false(isCPFOrCNPJ("11111111111"));
+  context.false(isCPFOrCNPJ("00000000000"));
+  context.false(isCPFOrCNPJ("111.111.111-11"));
+  context.false(isCPFOrCNPJ("UHASHUISIH910"));
+  context.false(isCPFOrCNPJ("366.41askaosoa8.768-70"));
+})
 
 test('isDate', (context) => {
   context.true(isDate('03/08/2017'));
