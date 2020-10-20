ac3c872dbc7c6e09055a7d62ca917d31985efc1e
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-10-20 17:12:25 -0300

    ✅ test parseToDate without seconds

--- test/parsers.test.ts
+++ test/parsers.test.ts
@@ -43,7 +43,7 @@ test('parseToDate: Throws errors on invalid pattern or date.', (context) => {
   );
 });
 
-test('parseToDate: Parses a brazilian formatted datetime to a Date instace', (context) => {
+test('parseToDate: Parses a brazilian formatted date and time to a Date instance', (context) => {
   const date = parseToDate('18/09/2019 22:45:38');
 
   context.is(date.getDate(), 18);
@@ -52,6 +52,16 @@ test('parseToDate: Parses a brazilian formatted datetime to a Date instace', (co
   context.is(date.getHours(), 22);
   context.is(date.getMinutes(), 45);
   context.is(date.getSeconds(), 38);
+});
+
+test('parseToDate: Can parse time without seconds', (context) => {
+  const date = parseToDate('21/06/2020 17:01');
+
+  context.is(date.getDate(), 21);
+  context.is(date.getMonth(), 5);
+  context.is(date.getFullYear(), 2020);
+  context.is(date.getHours(), 17);
+  context.is(date.getMinutes(), 1);
 });
 
 test('parseToNumber: Parses a brazilian formatted number to number', (context) => {
